77648ed7eea337b7af5d9de86fe1d00895902c2a
Tobias Kuhlmann <tobiasmartinkuhlmann@gmail.com>
2017-11-21 09:35:47 +0100

    after simons meeting 20.11

--- wlsev_estimation.py
+++ wlsev_estimation.py
@@ -96,17 +96,33 @@ class Wlsevestimation(object):
 
         # X = HodrickSum(X)/sigma2_t, no constant since constant is already in X
         # Initialize X(t) and divide by estimated sigma_t->t+1 for wls_ev
-        X = self.log_returns[self.forecast_horizon-1:-1].as_matrix() / est_var_dim_adj
+        #X = self.log_returns[self.forecast_horizon-1:-1].as_matrix() / est_var_dim_adj
+        X = self.log_returns[self.forecast_horizon - 1:-1].as_matrix()
 
         # Stack X and X(t-1) + X(t-2) + ... + X(t-(forecast horizon-1)) and divide each instance by estimated sigma_t->t+1 for wls_ev
-        for i in range(1, self.forecast_horizon-1):
-            X = np.vstack((X, self.log_returns[(self.forecast_horizon-(1+i)):-(1+i)].as_matrix() / est_var_dim_adj))
+        for i in range(1, self.forecast_horizon):
+            #X = np.vstack((X, self.log_returns[(self.forecast_horizon-(1+i)):-(1+i)].as_matrix() / est_var_dim_adj))
+            X = np.vstack((X, self.log_returns[(self.forecast_horizon - (1 + i)):-(1 + i)].as_matrix()))
 
         # Transpose X to get correct OLS dimensions
         X = np.transpose(X)
+        X = np.sum(X, axis = 1).reshape((X.shape[0], 1))
+
+        # Calculate variances for scaling
+        # Calculate Var(x_t_rolling): Variance of rolling sum
+        Var_x_t_rolling = X.var()
+        # Calculate Var(x_t): Variance of log return series x_t
+        Var_x_t = self.log_returns.var()
+
+        # Continue X sum
+        X = X / est_var_dim_adj.reshape((est_var_dim_adj.shape[0], 1))
+
+        # add OLS constant
+        X = sm.add_constant(X)
 
         # Y = r_(t+1)/sigma2_t
         Y = self.log_returns[self.forecast_horizon:].as_matrix() / est_var_dim_adj
+        Y = np.transpose(Y)
 
         # Next, run ols regression to estimate the wlsev parameters
         wlsev_reg_model = sm.OLS(Y, X)
@@ -121,12 +137,6 @@ class Wlsevestimation(object):
 
         # TODO: 2. Scale the resulting coefficients and standard errors
 
-        # Calculate Var(x_t_rolling): Variance of rolling sum
-        Var_x_t_rolling = X.var(0)
-
-        # Calculate Var(x_t): Variance of log return series x_t
-        Var_x_t = X.var(0)[0]
-
         # Scale Var_x_t_rolling/Var_x_t
         scale = Var_x_t_rolling / Var_x_t
 
@@ -136,13 +146,16 @@ class Wlsevestimation(object):
         # Scale standard errors
         std_errors = scale * robust_standard_errors.bse
 
+        # t-statistic
+        t_stats = betas / std_errors
+
         # Print Results
         print("Results")
         print("-------------------------------------------------------------------------------------------------------")
         print("Scales: {}".format(scale))
         print("Scaled betas: {}".format(betas))
         print("Scaled bse standard errors: {}".format(std_errors))
-        print("Scaled mse standard errors: {}".format(scale * robust_standard_errors.mse_resid))
+        print("Scaled t-stats: {}".format(t_stats))
         print("-------------------------------------------------------------------------------------------------------")
 
         return wlsev, robust_standard_errors
